b7a466d718546cb7a7c71d7899658b8584d30a71
linjama <matti.linjama@tut.fi>
2013-11-24 21:27:12 +0200

    Added MainPage controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pannuhuone::Application.routes.draw do
+  get "main_page/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
