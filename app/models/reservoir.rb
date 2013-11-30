7604c5915e8af8fe68442ed2aef303a038fce9f4
linjama <matti.linjama@tut.fi>
2013-11-30 19:27:37 +0200

    Added maximum capacities to model

--- app/models/reservoir.rb
+++ app/models/reservoir.rb
@@ -12,6 +12,7 @@ class Reservoir < ActiveRecord::Base
   TEMPERATURE_OF_INLET_WATER = 6
   TEMPERATURE_OF_HEATING_RETURN = 28  # Not planned to be measured
   MINIMUM_TEMPERATURE_FOR_HOT_WATER = 46
+  MAXIMUM_TEMPERATURE =85
   TEMPERATURE_MEASUREMENT_POCKETS = Vector[20, 20, 20, 20, 20]/100 
   
   def total_heat_capacity
@@ -22,12 +23,20 @@ class Reservoir < ActiveRecord::Base
     self.total_heat_capacity * TEMPERATURE_MEASUREMENT_POCKETS
   end
   
+  def maximum_capacity_for_heating
+    self.total_heat_capacity*(MAXIMUM_TEMPERATURE-TEMPERATURE_OF_HEATING_RETURN)
+  end
+  
   def remaining_capacity_for_heating
     temperature_differences = add_scalar_to_vector(
       self.read_temperatures, -TEMPERATURE_OF_HEATING_RETURN
     )
       
     vector_multiply(self.partial_heat_capacity, temperature_differences).reduce(:+)
+  end
+  
+  def maximum_capacity_for_hot_water
+    self.total_heat_capacity*(MAXIMUM_TEMPERATURE-MINIMUM_TEMPERATURE_FOR_HOT_WATER)
   end
   
   def remaining_capacity_for_hot_water
--- spec/models/reservoir_spec.rb
+++ spec/models/reservoir_spec.rb
@@ -14,21 +14,35 @@ describe "Reservoir" do
     reservoir.total_heat_capacity.should be < 100   # kWh/deg_C 
   end
   
-  specify ": The sum of partial heat capacities is equal to total heat capacity" do
+  specify "The sum of partial heat capacities is equal to total heat capacity" do
     sum = reservoir.partial_heat_capacity.reduce(:+)
     sum.should be_within(0.0001).of reservoir.total_heat_capacity
   end
   
-  specify ": Each temperature pocket has sensor" do
+  specify "Each temperature pocket has sensor" do
     reservoir.temperature_sensors.count.should eq Reservoir::TEMPERATURE_MEASUREMENT_POCKETS.size
     reservoir.read_temperatures.size.should eq Reservoir::TEMPERATURE_MEASUREMENT_POCKETS.size
   end
   
-  specify ": The temperature sensor readings are reasonable" do
+  specify "The temperature sensor readings are reasonable" do
     temperatures = reservoir.read_temperatures
     temperatures.each do |t|
       t.should be_within(45).of 55
     end
+  end
+  
+  it "has reasonable value for maximum capacity for hot water" do
+    max_hot_water_capacity = reservoir.maximum_capacity_for_hot_water
+    estimate = reservoir.total_heat_capacity*35
+    tolerance = reservoir.total_heat_capacity*10
+    max_hot_water_capacity.should be_within(tolerance).of estimate
+  end
+  
+  it "has reasonable value for maximum capacity for heating" do
+    max_heating_capacity = reservoir.maximum_capacity_for_heating
+    estimate = reservoir.total_heat_capacity*50
+    tolerance = reservoir.total_heat_capacity*10
+    max_heating_capacity.should be_within(tolerance).of estimate
   end
 
 end
