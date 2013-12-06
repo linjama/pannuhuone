66e94475259d8efe6c9afce2201f3fcedea29833
linjama <matti.linjama@tut.fi>
2013-12-06 12:00:00 +0200

    continued styling

--- app/helpers/main_page_helper.rb
+++ app/helpers/main_page_helper.rb
@@ -4,9 +4,12 @@ module MainPageHelper
     remaining_hot_water = @reservoir.remaining_capacity_for_hot_water
     max_hot_water = @reservoir.maximum_capacity_for_hot_water
     full_heigth = dimensions_reader("content-heigth").strip[0..-3].to_f
-    
+    section_width = dimensions_reader("section-width").strip[0..-3].to_f
+    section_padding = dimensions_reader("section-padding").strip[0..-3].to_f
+            
     opts = {
-      height: (full_heigth*remaining_hot_water/max_hot_water).ceil.to_s+"px" , 
+      height: (full_heigth*remaining_hot_water/max_hot_water).ceil.to_s+"px" ,
+      width: (section_width - 2 * section_padding).floor.to_s+"px" ,
       id: "hot_water_display_bar"
     }
     #options[:width] = dimensions_reader("hot-water-bar-width")
