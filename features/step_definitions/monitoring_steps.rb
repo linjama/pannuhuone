956b32f73b9a19dc40377ce25231418d36d847ff
linjama <matti.linjama@tut.fi>
2013-12-29 11:22:56 +0200

    moved old index.html.erb to overview.html.erb

---
+++ features/step_definitions/monitoring_steps.rb
@@ -0,0 +1,11 @@
+When(/^I visit the home page with my smartphone$/) do
+  visit '/'
+end
+
+Then(/^I see the exhaust gas temperature$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I see the boiler temperature$/) do
+  pending # express the regexp above with the code you wish you had
+end
